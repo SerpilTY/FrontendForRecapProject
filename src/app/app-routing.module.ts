ea0f830702ebe8b6712dee93c9fd540b551ef278
Serpil81 <44366185+Serpil81@users.noreply.github.com>
2022-09-13 20:12:17 +0300

    Project Week 19 *Final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CarDetailDtoComponent } from './components/car-detail-dto/car-detail-dto.component';
 import { CarComponent } from './components/car/car.component';
+import { PaymentComponent } from './components/payment/payment.component';
 
 const routes: Routes = [
 {path:"", pathMatch:"full", component:CarComponent},
@@ -9,7 +10,8 @@ const routes: Routes = [
 {path:"cars/brands/:brandId", component: CarComponent},
 {path:"cars/colors/:colorId", component: CarComponent},
 {path:"cars/brand:brandId/color:colorId",component:CarComponent},
-{path:"cars/cardetail/:carId",component:CarDetailDtoComponent}
+{path:"cars/cardetail/:carId",component:CarDetailDtoComponent},
+{path:"cardetails/payment",component:PaymentComponent},
 ];
 
 
