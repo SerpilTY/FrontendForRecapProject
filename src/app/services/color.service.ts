e3720f5c1b991fb3b80dae917bc12ee67478a56a
Serpil81 <44366185+Serpil81@users.noreply.github.com>
2022-09-20 23:24:54 +0300

    Project Week 20  *Adding Requirements Satisfied.

--- src/app/services/color.service.ts
+++ src/app/services/color.service.ts
@@ -3,17 +3,21 @@ import {HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Color } from '../models/color';
 import { ListResponseModel } from '../models/listResponseModel';
+import { ResponseModel } from '../models/responseModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ColorService {
 
-  apiUrl="https://localhost:44355/api/colors/getall";
+  apiUrl="https://localhost:44355/api/colors/";
   constructor(private httpClient: HttpClient) { }
 
   getColors():Observable<ListResponseModel<Color>> {
-    return this.httpClient.get<ListResponseModel<Color>>(this.apiUrl);
+    return this.httpClient.get<ListResponseModel<Color>>(this.apiUrl+"getall");
+    }
+    addColor(color: Color):Observable<ResponseModel>{
+      return this.httpClient.post<ResponseModel>(this.apiUrl+"add",color)
     }
 }
 
