3434da43a0ada461011f42009349a1dae3f86042
Serpil81 <44366185+Serpil81@users.noreply.github.com>
2022-10-14 14:02:53 +0300

    Project Week 21 *Navbar Updated.

--- src/app/components/navi/navi.component.ts
+++ src/app/components/navi/navi.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../../services/auth.service';
+import {User} from '../../models/user';
+import {LocalStorageService} from '../../services/local-storage.service';
+import {Router} from '@angular/router';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-navi',
@@ -8,9 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NaviComponent implements OnInit {
 
-  constructor() { }
+  userInfo: User = this.authService.getUser();
+
+  constructor(private authService: AuthService, private localStorageService: LocalStorageService, private router: Router, private toastrService:ToastrService) {
+  }
 
   ngOnInit(): void {
+  }
+
+
+  isAuthenticated() {
+    return this.authService.loggedIn();
+  }
+
+
+  logout() {
+    this.localStorageService.removeToken();
+    this.toastrService.success('Logged Out.')
+  }
+
+
+  ngDoCheck() { 
+    if (this.userInfo !== this.authService.user) {
+      this.userInfo = this.authService.user;
+    }
+
+  }
+
+  goToAdminEdit() {
+    this.router.navigate(['adminEdit']);
   }
 
 }
